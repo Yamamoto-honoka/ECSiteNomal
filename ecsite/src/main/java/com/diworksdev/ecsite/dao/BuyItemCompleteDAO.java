550e4a871601527b68f5ded5ae082f5f7293ae48
Yamamoto-honoka <kirakirahonoka@gmail.com>
2025-04-19 00:41:46 +0900

    4/19 修正し、05完了＆MyPageDTO＆MyPageDAO完成。残り06のみ。明日（明後日未明）には終わるかな…。ミスなければ。

--- ecsite/src/main/java/com/diworksdev/ecsite/dao/BuyItemCompleteDAO.java
+++ ecsite/src/main/java/com/diworksdev/ecsite/dao/BuyItemCompleteDAO.java
@@ -7,7 +7,7 @@ import com.diworksdev.ecsite.util.DBConnector;
 import com.diworksdev.ecsite.util.DateUtil;
 
 public class BuyItemCompleteDAO {
-	public void buyItemInfo(String item_transacton_id,
+	public void buyItemInfo(String item_transaction_id,
 							 String total_price,
 							 String total_count,
 							 String user_master_id,
@@ -15,7 +15,7 @@ public class BuyItemCompleteDAO {
 		DBConnector dbConnector = new DBConnector();
 		Connection connection = dbConnector.getConnection();
 		DateUtil dateUtil = new DateUtil();
-		String sql = "INSERT INTO user_buy_item_transacton"
+		String sql = "INSERT INTO user_buy_item_transaction"
 				+ "(item_transaction_id, total_price, total_count,"
 				+ "user_master_id, pay, insert_date)"
 				+ "VALUES(?, ?, ?, ?, ?, ?)";
@@ -23,7 +23,7 @@ public class BuyItemCompleteDAO {
 		try {
 			PreparedStatement preparedStatement = 
 					connection.prepareStatement(sql);
-			preparedStatement.setString(1, item_transacton_id);
+			preparedStatement.setString(1, item_transaction_id);
 			preparedStatement.setString(2, total_price);
 			preparedStatement.setString(3, total_count);
 			preparedStatement.setString(4, user_master_id);
