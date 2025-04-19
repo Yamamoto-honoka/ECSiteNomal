7829c949ead7ad8bb2813bbed7919257aeec6dbb
Yamamoto-honoka <kirakirahonoka@gmail.com>
2025-04-19 22:19:41 +0900

    4/19 MyPage&削除機能完成。空白ダイジ。

--- ecsite/src/main/java/com/diworksdev/ecsite/dao/MyPageDAO.java
+++ ecsite/src/main/java/com/diworksdev/ecsite/dao/MyPageDAO.java
@@ -17,12 +17,12 @@ public class MyPageDAO {
 	SQLException{
 		ArrayList<MyPageDTO> myPageDTO = new ArrayList<MyPageDTO>();
 		String sql = "SELECT ubit.id, iit.item_name, ubit.total_price,"
-				+ "ubit.total_count, ubit.pay, ubit.insert_date"
-				+ "FROM user_buy_item_transaction ubit"
-				+ "LEFT JOIN item_info_transaction iit"
-				+ "ON ubit.item_transaction_id = iit.id"
-				+ "WHERE ubit.item_transaction_id = ?"
-				+ "AND ubit.user_master_id = ?"
+				+ "ubit.total_count, ubit.pay, ubit.insert_date "
+				+ "FROM user_buy_item_transaction ubit "
+				+ "LEFT JOIN item_info_transaction iit "
+				+ "ON ubit.item_transaction_id = iit.id "
+				+ "WHERE ubit.item_transaction_id = ? "
+				+ "AND ubit.user_master_id = ? "
 				+ "ORDER BY insert_date DESC";
 		try {
 			PreparedStatement preparedStatement = connection.prepareStatement(sql);
@@ -53,7 +53,7 @@ public class MyPageDAO {
 	public int buyItemHistoryDelete
 	(String item_transaction_id, String user_master_id)throws
 	SQLException{
-		String sql = "DELETE FROM user_buy_item_transaction"
+		String sql = "DELETE FROM user_buy_item_transaction "
 				+ "WHERE item_transaction_id = ? AND user_master_id = ?";
 		PreparedStatement preparedStatement;
 		int result = 0;
